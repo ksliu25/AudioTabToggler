253a57aab20611ce8a8ef7f32d89ba3cc965a464
Kenneth Liu <ksliu25@gmail.com>
2017-03-26 19:46:10 -0500

    Add incognito option to manifest, hardcode Google Music's selector, update README

--- README.md
+++ README.md
@@ -4,11 +4,16 @@
 </p>
 
 <p>
-    Tested with Youtube, SoundCloud, anything with a single instance of an audio or video tag. Unreliably, it also features a straight document queryselector for those that dynamically generate HTMLAudioElements or use HTML5 Canvas in more modern audio players such as SoundCloud. <b>Also note that this will not work for anything that requires authentication -- such as Spotify's Web Player.</b>
+    Tested with Youtube, SoundCloud, Pandora, Google Music, anything with a single instance of an audio or video tag. Unreliably, it also features a straight document queryselector for those that dynamically generate HTMLAudioElements or use HTML5 Canvas in more modern audio players such as SoundCloud. <b>Also note that it seems this will not work for anything that requires authentication -- such as Spotify's Web Player.</b>
 </p>
 
 <h2> Installation</h2>
 <p> Download the zipped file, extract, load up Chrome Extensions and "Load unpacked extension..." pointed at the extracted directory </p>
-
 <h2> Usage </h2>
-<p> Alt + Shift + Tab # on Mac, Ctrl + Shift + Tab # on PC for only the FIRST 4 TABS as this is a limitation set forth by Chrome extension. I'm sure there are ways around it but it seems a lot more involved than what I needed, also are you listening to more than 4 different audio sources?! </p>
+<p> 
+    Alt + Shift + Tab # on Mac, Ctrl + Shift + Tab # on PC for only the FIRST 4 TABS as this is a limitation set forth by Chrome extension. I'm sure there are ways around it but it seems a lot more involved than what I needed, also are you listening to more than 4 different audio sources?! 
+</p>
+<p>
+    Additionally, you can set them yourself by navigating to here in your url bar:
+    chrome://extensions/configureCommands
+</p>
--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.commands.onCommand.addListener(function(command) {
     if (command == "toggle-audio-tab-1") {
         chrome.tabs.query({}, function(tabs){
             chrome.tabs.executeScript(tabs[0].id, {
-                'code': 'var video, audio, soundcloud, tal; video = document.querySelector("video"); audio = document.querySelector("audio"); soundcloud = document.querySelector("button.playControl.sc-ir.playControls__icon.playControls__play"); tal = document.querySelector("button[type=button]"); if (video) { video.paused ? video.play() : video.pause() }; if (audio) { audio.paused ? audio.play() : audio.pause() }; if (soundcloud) { soundcloud.click() }; if (tal) { tal.click() };'
+                'code': 'var video, audio, soundcloud, tal, googleplay; video = document.querySelector("video"); audio = document.querySelector("audio"); soundcloud = document.querySelector("button.playControl.sc-ir.playControls__icon.playControls__play"); tal = document.querySelector("button[type=button]"); googleplay = document.querySelector("paper-icon-button#player-bar-play-pause");if (video) { video.paused ? video.play() : video.pause() }; if (audio) { audio.paused ? audio.play() : audio.pause() }; if (soundcloud) { soundcloud.click() }; if (tal) { tal.click() }; if (googleplay) { googleplay.click() };'
             });
         });
     }
@@ -10,7 +10,7 @@ chrome.commands.onCommand.addListener(function(command) {
     if (command == "toggle-audio-tab-2") {
         chrome.tabs.query({}, function(tabs){
             chrome.tabs.executeScript(tabs[1].id, {
-                'code': 'var video, audio, soundcloud, tal; video = document.querySelector("video"); audio = document.querySelector("audio"); soundcloud = document.querySelector("button.playControl.sc-ir.playControls__icon.playControls__play"); tal = document.querySelector("button[type=button]"); if (video) { video.paused ? video.play() : video.pause() }; if (audio) { audio.paused ? audio.play() : audio.pause() }; if (soundcloud) { soundcloud.click() }; if (tal) { tal.click() };'
+                'code': 'var video, audio, soundcloud, tal, googleplay; video = document.querySelector("video"); audio = document.querySelector("audio"); soundcloud = document.querySelector("button.playControl.sc-ir.playControls__icon.playControls__play"); tal = document.querySelector("button[type=button]"); googleplay = document.querySelector("paper-icon-button#player-bar-play-pause");if (video) { video.paused ? video.play() : video.pause() }; if (audio) { audio.paused ? audio.play() : audio.pause() }; if (soundcloud) { soundcloud.click() }; if (tal) { tal.click() }; if (googleplay) { googleplay.click() };'
             });
         });
     }
@@ -18,7 +18,7 @@ chrome.commands.onCommand.addListener(function(command) {
     if (command == "toggle-audio-tab-3") {
         chrome.tabs.query({}, function(tabs){
             chrome.tabs.executeScript(tabs[2].id, {
-                'code': 'var video, audio, soundcloud, tal; video = document.querySelector("video"); audio = document.querySelector("audio"); soundcloud = document.querySelector("button.playControl.sc-ir.playControls__icon.playControls__play"); tal = document.querySelector("button[type=button]"); if (video) { video.paused ? video.play() : video.pause() }; if (audio) { audio.paused ? audio.play() : audio.pause() }; if (soundcloud) { soundcloud.click() }; if (tal) { tal.click() };'
+                'code': 'var video, audio, soundcloud, tal, googleplay; video = document.querySelector("video"); audio = document.querySelector("audio"); soundcloud = document.querySelector("button.playControl.sc-ir.playControls__icon.playControls__play"); tal = document.querySelector("button[type=button]"); googleplay = document.querySelector("paper-icon-button#player-bar-play-pause");if (video) { video.paused ? video.play() : video.pause() }; if (audio) { audio.paused ? audio.play() : audio.pause() }; if (soundcloud) { soundcloud.click() }; if (tal) { tal.click() }; if (googleplay) { googleplay.click() };'
             });
         });
     }
@@ -26,7 +26,7 @@ chrome.commands.onCommand.addListener(function(command) {
     if (command == "toggle-audio-tab-4") {
         chrome.tabs.query({}, function(tabs){
             chrome.tabs.executeScript(tabs[3].id, {
-                'code': 'var video, audio, soundcloud, tal; video = document.querySelector("video"); audio = document.querySelector("audio"); soundcloud = document.querySelector("button.playControl.sc-ir.playControls__icon.playControls__play"); tal = document.querySelector("button[type=button]"); if (video) { video.paused ? video.play() : video.pause() }; if (audio) { audio.paused ? audio.play() : audio.pause() }; if (soundcloud) { soundcloud.click() }; if (tal) { tal.click() };'
+                'code': 'var video, audio, soundcloud, tal, googleplay; video = document.querySelector("video"); audio = document.querySelector("audio"); soundcloud = document.querySelector("button.playControl.sc-ir.playControls__icon.playControls__play"); tal = document.querySelector("button[type=button]"); googleplay = document.querySelector("paper-icon-button#player-bar-play-pause");if (video) { video.paused ? video.play() : video.pause() }; if (audio) { audio.paused ? audio.play() : audio.pause() }; if (soundcloud) { soundcloud.click() }; if (tal) { tal.click() }; if (googleplay) { googleplay.click() };'
             });
         });
     }
